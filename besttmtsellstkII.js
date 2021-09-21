ceb6a5003097934a82e10ba548c78a3e59c6a8bf
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-21 15:46:35 -0600

    Create besttmtsellstkII.js

---
+++ besttmtsellstkII.js
@@ -0,0 +1,14 @@
+var maxProfit = function(prices) {
+    let ans = 0;
+        for (let i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) ans += prices[i] - prices[i - 1];
+        }
+        return ans;
+   
+  /*
+  Success
+Details 
+Runtime: 72 ms, faster than 89.08% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
+Memory Usage: 40.2 MB, less than 51.15% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
+  
+  */
